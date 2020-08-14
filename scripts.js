5d21473622da646ae24d7f0e18e9f8286bea04c1
Tim Huang <tim@MacBook-Pro.local>
2020-08-14 23:15:16 +0800

    將一些watch改用computed

--- scripts.js
+++ scripts.js
@@ -1,83 +1,86 @@
 const app = Vue.createApp({
-  data() {
-      return {
-          uBikeStops: [],
-          filterStops: [],
-          pageSize: 10,
-          pageIndex: 0,
-          totalPages: 0,
-          pageRange: 5,
-          pageNumbers: [],
-          bempSortClass: '',
-          totSortClass: '',
-          isBempAsc: true,
-          isTotAsc: true,
-          searchName: '',
-      }
-  },
-  created() {
-      fetch('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz')
-          .then(res => res.json())
-          .then(json => {
-              const stops = Object.keys(json.retVal).map(key => json.retVal[key]);
-              this.filterStops = this.uBikeStops = stops;
-          });
-  },
-  watch: {
-      searchName() {
-          this.filterStops = this.uBikeStops.filter(stop => stop.sna.includes(this.searchName));
-      },
-      filterStops() {
-          this.totalPages = Math.ceil(this.filterStops.length / this.pageSize);
-          this.pageRange = this.totalPages < 5 ? this.totalPages : 5;
-          this.pageNumbers = Array(this.pageRange).fill().map((_, i) => i + 1);
-          this.gotoPage(1)
-      },
-      pageIndex() {
-          this.setPageNumber();
-      },
-      isBempAsc() {
-          this.totSortClass = 'fa-sort';
-          this.bempSortClass = this.isBempAsc ? 'fa-sort-asc' : 'fa-sort-desc';
-      },
-      isTotAsc() {
-          this.bempSortClass = 'fa-sort';
-          this.totSortClass = this.isTotAsc ? 'fa-sort-asc' : 'fa-sort-desc';
-      }
-  },
-  computed: {
-      paginationItems() {
-          const start = (this.pageIndex - 1) * this.pageSize;
+    data() {
+        return {
+            uBikeStops: [],
+            pageSize: 10,
+            pageIndex: 0,
+            pageNumbers: [],
+            searchName: '',
+            currentSort: '',
+            isSortDesc: true,
+        }
+    },
+    created() {
+        fetch('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz')
+            .then(res => res.json())
+            .then(json => {
+                const stops = Object.keys(json.retVal).map(key => json.retVal[key]);
+                this.uBikeStops = stops;
+                this.pageIndex = 1;
+            });
+    },
+    watch: {
+        pageIndex() {
+            this.setPageNumbers();
+        }
+    },
+    computed: {
+        paginationItems() {
+            const start = (this.pageIndex - 1) * this.pageSize;
 
-          return this.filterStops.slice(start, start + this.pageSize);
-      },
-  },
-  methods: {
-      timeFormat(val) {
-          const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
+            return this.sortedUBikeStops.slice(start, start + this.pageSize);
+        },
+        bempSortClass() {
+            return this.currentSort === 'bemp' ? this.isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc' : 'fa-sort';
+        },
+        totSortClass() {
+            return this.currentSort === 'tot' ? this.isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc' : 'fa-sort';
+        },
+        totalPages() {
+            return Math.ceil(this.filterUBikeStops.length / this.pageSize);
+        },
+        pageRange() {
+            let range = this.totalPages < 5 ? this.totalPages : 5;
+            this.pageNumbers = Array(range).fill().map((_, i) => i + 1);
 
-          return val.replace(pattern, '$1/$2/$3 $4:$5:$6')
-      },
-      sortBikeStops(column, isAsc) {
-          this.filterStops.sort((stop1, stop2) =>
-              isAsc ? stop1[column] - stop2[column] : stop2[column] - stop1[column])
-      },
-      gotoPage(index) {
-          if (index > 0 && index <= this.totalPages) {
-              this.pageIndex = index;
-          }
-      },
-      setPageNumber() {
-          const start = {
-              [this.pageNumbers[this.pageRange - 1]]: this.pageNumbers[0] + 1,
-              [this.pageNumbers[0]]: this.pageNumbers[0] - 1,
-              [1]: 1,
-              [this.totalPages]: this.totalPages - this.pageRange + 1
-          }[this.pageIndex];
+            return range;
+        },
+        filterUBikeStops() {
+            return this.uBikeStops.filter(stop => stop.sna.includes(this.searchName));
+        },
+        sortedUBikeStops() {
+            const column = this.currentSort;
 
-          if (start) {
-              this.pageNumbers = this.pageNumbers.map((_, i) => start + i);
-          }
-      }
-  }
+            return this.filterUBikeStops.sort((stop1, stop2) =>
+                this.isSortDesc ? stop2[column] - stop1[column] : stop1[column] - stop2[column])
+        }
+    },
+    methods: {
+        timeFormat(val) {
+            const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
+
+            return val.replace(pattern, '$1/$2/$3 $4:$5:$6')
+        },
+        gotoPage(index) {
+            if (index > 0 && index <= this.totalPages) {
+                this.pageIndex = index;
+            }
+        },
+        setPageNumbers() {
+            const start = {
+                [this.pageNumbers[this.pageRange - 1]]: this.pageNumbers[0] + 1,
+                [this.pageNumbers[0]]: this.pageNumbers[0] - 1,
+                [1]: 1,
+                [this.totalPages]: this.totalPages - this.pageRange + 1
+            }[this.pageIndex];
+
+            if (start) {
+                this.pageNumbers = this.pageNumbers.map((_, i) => start + i);
+            }
+        },
+        sort(column) {
+            this.currentSort = column;
+            this.isSortDesc = !this.isSortDesc;
+        }
+    }
 }).mount('#app');
